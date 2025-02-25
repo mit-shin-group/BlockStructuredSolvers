f7d356d372a8657bd7b8f4ede49e9d1ef324b887
jindavid <jindavid@mit.edu>
2025-02-24 22:33:52 -0500

    benchmark done

--- benchmark.jl
+++ benchmark.jl
@@ -5,33 +5,11 @@ using HSL
 
 import Pkg
 include("TriDiagBlockNestedv2.jl")
-import .TriDiagBlockNested: TriDiagBlockDataNested, initialize, factorize, solve
+include("benchmark_utils.jl")
+
+using Statistics, Printf
 
 ######
-
-function construct_block_tridiagonal(A_list, B_list)
-    N, n, _ = size(A_list)
-    blocks = Matrix{Float64}[]
-
-    # Construct the block matrix row-wise
-    for i = 1:N
-        row_blocks = Any[]
-        for j = 1:N
-            if i == j
-                push!(row_blocks, A_list[i, :, :])  # Diagonal blocks
-            elseif j == i + 1
-                push!(row_blocks, B_list[i, :, :])  # Upper diagonal blocks
-            elseif j == i - 1
-                push!(row_blocks, B_list[j, :, :]' )  # Lower diagonal blocks (transpose)
-            else
-                push!(row_blocks, zeros(n, n))  # Zero blocks elsewhere
-            end
-        end
-        push!(blocks, hcat(row_blocks...))
-    end
-
-    return vcat(blocks...)
-end
 
 n = 50 # size of each block
 m = 2 # number of blocks between separators
@@ -39,78 +17,122 @@ N = 55 # number of diagonal blocks
 P = Int((N + m) / (m+1)) # number of separators
 level = 3; # number of nested level
 
-#######################################
-A_list = zeros(N, n, n);
-for i = 1:N
-    temp = randn(Float64, n, n)
-    A_list[i, :, :] = temp * temp' + n * I
+
+# for i = 1:1000
+
+#     BigMatrix, d, x_true, A_list, B_list = generate_tridiagonal_system(N, n);
+
+#     #################################################
+
+#     BigMatrix_57 = Ma57(BigMatrix);
+
+#     @time ma57_factorize!(BigMatrix_57);
+#     @time x = ma57_solve(BigMatrix_57, d);
+
+#     norm(x - x_true)
+
+#     #################################################
+
+#     BigMatrix_sparse = SparseMatrixCSC(BigMatrix);
+
+#     @time F = cholesky(BigMatrix_sparse);
+
+#     @time x = F \ d;
+
+#     norm(x - x_true)
+
+#     #################################################
+
+#     @time LDLT = ldl(BigMatrix);  # LDLᵀ factorization of A
+
+#     @time x = LDLT \ d;  # solves Ax = b
+
+#     norm(x - x_true)
+
+#     #*********************
+#     data = initialize(N, m, n, P, A_list, B_list, level);
+
+#     @time factorize(data);
+
+#     x = zeros(data.N * n);
+
+#     @time solve(data, d, x)
+
+#     norm(x - x_true)
+
+# end
+
+function benchmark_factorization_and_solve(N, n, m, P, level, iter)
+    # Storage for times
+    ma57_factor_times = Float64[]
+    ma57_solve_times = Float64[]
+
+    chol_factor_times = Float64[]
+    chol_solve_times = Float64[]
+
+    ldl_factor_times = Float64[]
+    ldl_solve_times = Float64[]
+
+    custom_factor_times = Float64[]
+    custom_solve_times = Float64[]
+
+    for i = 1:iter
+        # Generate problem instance
+        BigMatrix, d, x_true, A_list, B_list = generate_tridiagonal_system(N, n)
+
+        #################################################
+        # **Method 1: MA57 Solver**
+        #################################################
+        BigMatrix_57 = Ma57(BigMatrix)
+
+        ma57_factor_time = @elapsed ma57_factorize!(BigMatrix_57)
+        ma57_solve_time = @elapsed x = ma57_solve(BigMatrix_57, d)
+
+        push!(ma57_factor_times, ma57_factor_time)
+        push!(ma57_solve_times, ma57_solve_time)
+
+        #################################################
+        # **Method 2: Cholesky Factorization**
+        #################################################
+        BigMatrix_sparse = SparseMatrixCSC(BigMatrix)
+
+        chol_factor_time = @elapsed F = cholesky(BigMatrix_sparse)
+        chol_solve_time = @elapsed x = F \ d
+
+        push!(chol_factor_times, chol_factor_time)
+        push!(chol_solve_times, chol_solve_time)
+
+        #################################################
+        # **Method 3: LDLᵀ Factorization**
+        #################################################
+        ldl_factor_time = @elapsed LDLT = ldl(BigMatrix)
+        ldl_solve_time = @elapsed x = LDLT \ d
+
+        push!(ldl_factor_times, ldl_factor_time)
+        push!(ldl_solve_times, ldl_solve_time)
+
+        #################################################
+        # **Method 4: Custom Solver**
+        #################################################
+        data = initialize(N, m, n, P, A_list, B_list, level)
+
+        custom_factor_time = @elapsed factorize(data)
+
+        x = zeros(data.N * n)
+        custom_solve_time = @elapsed solve(data, d, x)
+
+        push!(custom_factor_times, custom_factor_time)
+        push!(custom_solve_times, custom_solve_time)
+    end
+
+    # Compute and print the average times
+    println("Average Factorization and Solve Times over ", iter, " Runs:")
+    println("---------------------------------------------------")
+    @printf("MA57 - Factorize: %.6fs, Solve: %.6fs\n", mean(ma57_factor_times), mean(ma57_solve_times))
+    @printf("Cholesky - Factorize: %.6fs, Solve: %.6fs\n", mean(chol_factor_times), mean(chol_solve_times))
+    @printf("LDLᵀ - Factorize: %.6fs, Solve: %.6fs\n", mean(ldl_factor_times), mean(ldl_solve_times))
+    @printf("Custom - Factorize: %.6fs, Solve: %.6fs\n", mean(custom_factor_times), mean(custom_solve_times))
 end
 
-B_list = zeros(N-1, n, n);
-for i = 1:N-1
-    temp = randn(Float64, n, n)
-    B_list[i, :, :] = temp
-end
 
-x_true = rand(N, n);
-d_list = zeros(N, n);
-d_list[1, :] = A_list[1, :, :] * x_true[1, :] + B_list[1, :, :] * x_true[2, :];
-
-@views for i = 2:N-1
-
-    d_list[i, :] = B_list[i-1, :, :]' * x_true[i-1, :] + A_list[i, :, :] * x_true[i, :] + B_list[i, :, :] * x_true[i+1, :];
-
-end
-
-d_list[N, :] = B_list[N-1, :, :]' * x_true[N-1, :] + A_list[N, :, :] * x_true[N, :];
-
-d = zeros(N * n);
-
-@views for i = 1:N
-    
-    d[(i-1)*n+1:i*n] = d_list[i, :]
-
-end
-
-x_true = reshape(x_true', N*n);
-
-BigMatrix = construct_block_tridiagonal(A_list, B_list);
-
-
-#################################################
-
-BigMatrix_57 = Ma57(BigMatrix);
-
-@time ma57_factorize!(BigMatrix_57);
-@time x = ma57_solve(BigMatrix_57, d);
-
-norm(x - x_true)
-
-#################################################
-
-BigMatrix_sparse = SparseMatrixCSC(BigMatrix);
-
-@time F = cholesky(BigMatrix_sparse);
-
-@time x = F \ d;
-
-norm(x - x_true)
-
-#################################################
-
-@time LDLT = ldl(BigMatrix);  # LDLᵀ factorization of A
-
-@time x = LDLT \ d;  # solves Ax = b
-
-norm(x - x_true)
-
-#*********************
-data = initialize(N, m, n, P, A_list, B_list, level);
-
-@time factorize(data);
-
-x = zeros(data.N * n);
-
-@time solve(data, d, x)
-
-norm(x - x_true)
+benchmark_factorization_and_solve(N, n, m, P, level, 100)
---
+++ benchmark_utils.jl
@@ -0,0 +1,68 @@
+export construct_block_tridiagonal
+
+function construct_block_tridiagonal(A_list, B_list)
+    N, n, _ = size(A_list)
+    blocks = Matrix{Float64}[]
+
+    # Construct the block matrix row-wise
+    for i = 1:N
+        row_blocks = Any[]
+        for j = 1:N
+            if i == j
+                push!(row_blocks, A_list[i, :, :])  # Diagonal blocks
+            elseif j == i + 1
+                push!(row_blocks, B_list[i, :, :])  # Upper diagonal blocks
+            elseif j == i - 1
+                push!(row_blocks, B_list[j, :, :]' )  # Lower diagonal blocks (transpose)
+            else
+                push!(row_blocks, zeros(n, n))  # Zero blocks elsewhere
+            end
+        end
+        push!(blocks, hcat(row_blocks...))
+    end
+
+    return vcat(blocks...)
+end
+
+function generate_tridiagonal_system(N::Int, n::Int)
+    # Generate A_list (diagonal block matrices)
+    A_list = zeros(N, n, n)
+    for i in 1:N
+        temp = randn(Float64, n, n)
+        A_list[i, :, :] = temp * temp' + n * I
+    end
+
+    # Generate B_list (off-diagonal block matrices)
+    B_list = zeros(N-1, n, n)
+    for i in 1:N-1
+        temp = randn(Float64, n, n)
+        B_list[i, :, :] = temp
+    end
+
+    # Generate x_true
+    x_true = rand(N, n)
+
+    # Compute d_list
+    d_list = zeros(N, n)
+    d_list[1, :] = A_list[1, :, :] * x_true[1, :] + B_list[1, :, :] * x_true[2, :]
+
+    @views for i in 2:N-1
+        d_list[i, :] = B_list[i-1, :, :]' * x_true[i-1, :] + A_list[i, :, :] * x_true[i, :] + B_list[i, :, :] * x_true[i+1, :]
+    end
+
+    d_list[N, :] = B_list[N-1, :, :]' * x_true[N-1, :] + A_list[N, :, :] * x_true[N, :]
+
+    # Flatten d_list into a vector
+    d = zeros(N * n)
+    @views for i in 1:N
+        d[(i-1)*n+1:i*n] = d_list[i, :]
+    end
+
+    # Reshape x_true to a vector
+    x_true = reshape(x_true', N*n)
+
+    # Construct the block tridiagonal matrix
+    BigMatrix = construct_block_tridiagonal(A_list, B_list)
+
+    return BigMatrix, d, x_true, A_list, B_list
+end
