bed7d0f5ecbb01850725819bc860a73c37bc7add
jindavid <daaaaavjd@gmail.com>
2025-03-27 20:35:29 -0400

    algorithm works for arbitrary separators but performance unstable

--- src/block_cholesky_factor_solve.jl
+++ src/block_cholesky_factor_solve.jl
@@ -8,7 +8,7 @@ struct BlockTriDiagData{
     n::Int
     P::Int
 
-    I_separator::StepRange{Int64, Int64}
+    I_separator::Vector{Int}
     I_non_separator::Vector{Int}
 
     A_list::MT
@@ -30,14 +30,43 @@ struct BlockTriDiagData{
 
 end
 
-function initialize(N, m, n, P, A_list_final, B_list_final, level)
+function initialize(N, n, A_list_final, B_list_final, parallel)
 
     data = nothing;
-    T = typeof(A_list_final[1]);
+    level = 0;
+    N_list = [];
+    P_list = [];
+    m_list = [];
+    I_separator_list = [];
+    
+    while N > 10
+
+        m = 4
+        I_separator = 1:(m+1):N        
+        if I_separator[end] != N
+            I_separator = [I_separator; N]
+        else    
+            I_separator = [I_separator[1:end-1]; N]
+        end
+        
+        P = length(I_separator)
+        pushfirst!(N_list, N);
+        pushfirst!(P_list, P);
+        pushfirst!(m_list, m);
+        pushfirst!(I_separator_list, I_separator);
+        println("N = $N, P = $P, m = $m")
+        N = P
+        level += 1;
+
+    end
     
     for i = 1:level
 
-        I_separator = 1:(m+1):N
+        P = P_list[i]
+        N = N_list[i]
+        m = m_list[i]
+       
+        I_separator = I_separator_list[i]
         I_non_separator = setdiff(1:N, I_separator)
 
         # TODO slow?
@@ -51,12 +80,12 @@ function initialize(N, m, n, P, A_list_final, B_list_final, level)
 
         RHS = [zero(similar(A_list_final[1], n, 1)) for i in 1:P];
 
-        factor_list = [similar(A_list_final[1], n, 2*n) for j = 1:m*(P-1)];
+        factor_list = [similar(A_list_final[1], n, 2*n) for j = 1:N];
         temp_list = [zero(similar(A_list_final[1], 2*n, 1)) for i = 1:P-1];
         temp_B_list = [similar(A_list_final[1], n, n) for i = 1:2*(P-1)];
 
         M_2n_list = [zero(similar(A_list_final[1], 2*n, 2*n)) for i in 1:P-1];
-        factor_list_temp = [zero(similar(A_list_final[1], n, 2*n)) for i in 1:m*(P-1)];
+        factor_list_temp = [zero(similar(A_list_final[1], n, 2*n)) for i in 1:N];
 
         if i == level
             A_list = A_list_final
@@ -110,9 +139,9 @@ function factorize!(data::BlockTriDiagData)
     temp_B_list = data.temp_B_list
 
     # Copy data for factorization
-    temp_B_list[1:2:end] .= B_list[I_separator[1:P-1]]
-    temp_B_list[2:2:end] .= B_list[I_separator[2:P].-1]
-    LHS_A_list .+= A_list[I_separator]
+    copy_vector_of_arrays!(view(temp_B_list, 1:2:2*(P-1)), view(B_list, I_separator[1:P-1]))
+    copy_vector_of_arrays!(view(temp_B_list, 2:2:2*(P-1)), view(B_list, I_separator[2:P].-1))
+    add_vector_of_arrays!(LHS_A_list, view(A_list, I_separator))
 
     # Main factorization loop
     compute_schur_complement!(A_list, B_list, LHS_A_list, LHS_B_list, temp_B_list, factor_list, factor_list_temp, M_2n_list, I_separator, P, m, n)
@@ -138,10 +167,9 @@ function solve!(data::BlockTriDiagData, d_list, x)
 
     # Copy d_list to RHS
     copy_vector_of_arrays!(RHS, view(d_list, I_separator))
-    d_list_non_separator = view(d_list, I_non_separator)
 
     # Compute RHS from Schur complement
-    compute_schur_rhs!(data.factor_list, d_list_non_separator, data.temp_list, RHS, P, m, n)
+    compute_schur_rhs!(data.factor_list, d_list, data.temp_list, RHS, I_separator, P, m, n)
 
     # Solve system
     if isnothing(data.NextData)
@@ -155,9 +183,9 @@ function solve!(data::BlockTriDiagData, d_list, x)
     update_boundary_solution!(data.temp_B_list, x, d_list, I_separator, P)
 
     # Solve for non-separators
-    solve_non_separator_blocks!(data.A_list, data.B_list, d_list_non_separator, I_separator, P, m)
+    solve_non_separator_blocks!(data.A_list, data.B_list, d_list, I_separator, P, m)
 
-    copy_vector_of_arrays!(view(x, I_non_separator), d_list_non_separator)
+    copy_vector_of_arrays!(view(x, I_non_separator), view(d_list, I_non_separator))
 
     return nothing
 end
