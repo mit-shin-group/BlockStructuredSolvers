6c85546fcdb9d863d604c67e0d11d9020ac0e2e6
jindavid <daaaaavjd@gmail.com>
2025-05-09 16:50:20 -0400

    updated benchmark

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -12,6 +12,7 @@ include("utils.jl")
 
 using Printf, ProgressBars, Statistics
 
+device!(1)
 
 ######
 
@@ -22,28 +23,28 @@ n = 32 # size of each block
 seed = 42 # random seed for reproducibility
 
 function run(N, n)
+    # Set random seed for reproducibility
+    Random.seed!(seed)
 
     A_list, B_list, x_list, x, d_list = generate_data(N, n)
     A_list_gpu, B_list_gpu, x_list_gpu, x_gpu, d_list_gpu = to_gpu(A_list, B_list, x_list, x, d_list)
     BigMatrix, d = construct_block_tridiagonal(A_list, B_list, d_list)
 
+    # MA57
     BigMatrix_57 = Ma57(BigMatrix)
     d_57 = deepcopy(d)
     ma57_factorize_time = @elapsed ma57_factorize!(BigMatrix_57)
     ma57_solve_time = @elapsed ma57_solve(BigMatrix_57, d_57)
-
     BigMatrix_57 = nothing
 
     # CHOLMOD
     chol_factor_time = @elapsed F = cholesky(BigMatrix)
     chol_solve_time = @elapsed F \ d
-
     F = nothing
 
     # LDLFactorizations 
     ldl_factor_time = @elapsed LDLT = ldl(BigMatrix; P=Vector(1:N*n))
     ldl_solve_time = @elapsed LDLT \ d
-
     LDLT = nothing
 
     # CUDSS
@@ -55,20 +56,54 @@ function run(N, n)
     # Time CUDSS solve using events
     x_cudss = deepcopy(d_cudss)
     cudss_solve_time = CUDA.@elapsed blocking=true cudss("solve", chol, x_cudss, d_cudss)
-
     BigMatrix_cudss = nothing
     chol = nothing
 
-    # Warmup factorization
-    data = initialize(N, n, A_list, B_list, true)
-    data_gpu = initialize(N, n, A_list_gpu, B_list_gpu, true)
-    data_sequential = initialize(N, n, A_list, B_list)
-    cpu_factorize_time = @elapsed factorize!(data)
-    cpu_solve_time = @elapsed solve!(data, d_list, x)
+    # Create GPU tensors for our algorithms
+    A_tensor_gpu = CuArray{Float64}(undef, n, n, N)
+    B_tensor_gpu = CuArray{Float64}(undef, n, n, N-1)
+    d_tensor_gpu = CuArray{Float64}(undef, n, 1, N)
+    
+    # Fill the GPU tensors
+    @CUDA.allowscalar for i in 1:N
+        A_tensor_gpu[:, :, i] = A_list_gpu[i]
+    end
+    
+    @CUDA.allowscalar for i in 1:N-1
+        B_tensor_gpu[:, :, i] = B_list_gpu[i]
+    end
+    
+    @CUDA.allowscalar for i in 1:N
+        d_tensor_gpu[:, :, i] = d_list_gpu[i]
+    end
+    
+    # Initialize batched solver
+    data_gpu = initialize_batched(N, n)
+    # Copy data to GPU tensors
+    copyto!(data_gpu.A_tensor, A_tensor_gpu)
+    copyto!(data_gpu.B_tensor, B_tensor_gpu)
+    copyto!(data_gpu.d_tensor, d_tensor_gpu)
+    
+    # Initialize sequential solver
+    data_sequential = initialize_seq(N, n)
+    # Copy data to GPU tensors
+    copyto!(data_sequential.A_tensor, A_tensor_gpu)
+    copyto!(data_sequential.B_tensor, B_tensor_gpu)
+    copyto!(data_sequential.d_tensor, d_tensor_gpu)
+    
+    # Warmup
+    CUDA.@sync factorize!(data_gpu)
+    CUDA.@sync solve!(data_gpu)
+    CUDA.@sync factorize!(data_sequential)
+    CUDA.@sync solve!(data_sequential)
+    
+    # Benchmark our GPU batched algorithm
     gpu_factorize_time = @elapsed CUDA.@sync factorize!(data_gpu)
-    gpu_solve_time = @elapsed CUDA.@sync solve!(data_gpu, d_list_gpu, x_gpu)
-    sequential_factorize_time = @elapsed factorize!(data_sequential)
-    sequential_solve_time = @elapsed solve!(data_sequential, d_list, x)
+    gpu_solve_time = @elapsed CUDA.@sync solve!(data_gpu)
+    
+    # Benchmark our sequential GPU algorithm
+    sequential_factorize_time = @elapsed CUDA.@sync factorize!(data_sequential) 
+    sequential_solve_time = @elapsed CUDA.@sync solve!(data_sequential)
     
     GC.gc()
     CUDA.reclaim()
@@ -81,8 +116,6 @@ function run(N, n)
     ldl_solve_time, 
     cudss_factor_time, 
     cudss_solve_time, 
-    cpu_factorize_time, 
-    cpu_solve_time, 
     gpu_factorize_time, 
     gpu_solve_time, 
     sequential_factorize_time, 
@@ -106,9 +139,6 @@ function benchmark_factorization_and_solve(iter)
     cudss_factor_times = Float64[]
     cudss_solve_times = Float64[]
 
-    cpu_factor_times = Float64[]
-    cpu_solve_times = Float64[]
-
     gpu_factor_times = Float64[]
     gpu_solve_times = Float64[]
 
@@ -119,7 +149,7 @@ function benchmark_factorization_and_solve(iter)
 
     _ = run(20, 32)
 
-    (N, n) = (100, 256)
+    (N, n) = (51, 129)
 
     println("Starting benchmark...")
 
@@ -139,14 +169,11 @@ function benchmark_factorization_and_solve(iter)
         push!(cudss_factor_times, results[7])
         push!(cudss_solve_times, results[8])
 
-        push!(cpu_factor_times, results[9])
-        push!(cpu_solve_times, results[10])
+        push!(gpu_factor_times, results[9])
+        push!(gpu_solve_times, results[10])
 
-        push!(gpu_factor_times, results[11])
-        push!(gpu_solve_times, results[12])
-
-        push!(sequential_factor_times, results[13])
-        push!(sequential_solve_times, results[14])
+        push!(sequential_factor_times, results[11])
+        push!(sequential_solve_times, results[12])
     end
 
     # Compute and print the average times
@@ -156,9 +183,9 @@ function benchmark_factorization_and_solve(iter)
     @printf("CHOLMOD - Factorize: %.6f ms, Solve: %.6f ms\n", mean(chol_factor_times) * 1000, mean(chol_solve_times) * 1000)
     @printf("LDLFactorizations - Factorize: %.6f ms, Solve: %.6f ms\n", mean(ldl_factor_times) * 1000, mean(ldl_solve_times) * 1000)
     @printf("CUDSS - Factorize: %.6f ms, Solve: %.6f ms\n", mean(cudss_factor_times) * 1000, mean(cudss_solve_times) * 1000)
-    @printf("CPU - Factorize: %.6f ms, Solve: %.6f ms\n", mean(cpu_factor_times) * 1000, mean(cpu_solve_times) * 1000)
-    @printf("GPU - Factorize: %.6f ms, Solve: %.6f ms\n", mean(gpu_factor_times) * 1000, mean(gpu_solve_times) * 1000)
-    @printf("Sequential - Factorize: %.6f ms, Solve: %.6f ms\n", mean(sequential_factor_times) * 1000, mean(sequential_solve_times) * 1000)
+    @printf("GPU Batched - Factorize: %.6f ms, Solve: %.6f ms\n", mean(gpu_factor_times) * 1000, mean(gpu_solve_times) * 1000)
+    @printf("GPU Sequential - Factorize: %.6f ms, Solve: %.6f ms\n", mean(sequential_factor_times) * 1000, mean(sequential_solve_times) * 1000)
+    
     # Also print standard deviations
     println("\nStandard Deviations:")
     println("---------------------------------------------------")
@@ -166,9 +193,8 @@ function benchmark_factorization_and_solve(iter)
     @printf("CHOLMOD - Factorize: %.6f ms, Solve: %.6f ms\n", std(chol_factor_times) * 1000, std(chol_solve_times) * 1000)
     @printf("LDLFactorizations - Factorize: %.6f ms, Solve: %.6f ms\n", std(ldl_factor_times) * 1000, std(ldl_solve_times) * 1000)
     @printf("CUDSS - Factorize: %.6f ms, Solve: %.6f ms\n", std(cudss_factor_times) * 1000, std(cudss_solve_times) * 1000)
-    @printf("CPU - Factorize: %.6f ms, Solve: %.6f ms\n", std(cpu_factor_times) * 1000, std(cpu_solve_times) * 1000)
-    @printf("GPU - Factorize: %.6f ms, Solve: %.6f ms\n", std(gpu_factor_times) * 1000, std(gpu_solve_times) * 1000)
-    @printf("Sequential - Factorize: %.6f ms, Solve: %.6f ms\n", std(sequential_factor_times) * 1000, std(sequential_solve_times) * 1000)
+    @printf("GPU Batched - Factorize: %.6f ms, Solve: %.6f ms\n", std(gpu_factor_times) * 1000, std(gpu_solve_times) * 1000)
+    @printf("GPU Sequential - Factorize: %.6f ms, Solve: %.6f ms\n", std(sequential_factor_times) * 1000, std(sequential_solve_times) * 1000)
 end
 
 # Run benchmark with warmup
