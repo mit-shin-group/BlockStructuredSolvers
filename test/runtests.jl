7d4f7b8aa1e611f61be9be65a47cffe58cbf4fd9
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-05-13 23:45:22 -0500

    Add support for AMD GPUs

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,24 +3,14 @@ using BlockStructuredSolvers
 import LinearAlgebra: I
 using SparseArrays
 
-import CUDA: CuMatrix, DeviceMemory, functional
+import CUDA: CuMatrix, DeviceMemory
 
 # Set random seed for deterministic tests
 using Random
 Random.seed!(42)
 
-# Check if CUDA tests should be run based on command line arguments
-const RUN_CUDA_TESTS = "cuda=true" in ARGS
-
-# Skip CUDA tests if not supported or if explicitly disabled
-if RUN_CUDA_TESTS && !functional()
-    @warn "CUDA is requested for testing but not functional on this system. CUDA tests will be skipped."
-    cuda_enabled = false
-else
-    cuda_enabled = RUN_CUDA_TESTS
-end
-
 include("utils.jl")
 include("test_cuda.jl")
+include("test_rocm.jl")
 # include("test_sequential_cholesky_factor.jl")
 # include("test_CSR_interface.jl")
