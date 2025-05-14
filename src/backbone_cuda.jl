e07a53a4d99bbaded77bbf5add0b0a0619c1e1a3
jindavid <daaaaavjd@gmail.com>
2025-05-13 23:40:54 -0400

    add meta programming for float32

--- src/backbone_cuda.jl
+++ src/backbone_cuda.jl
@@ -27,102 +27,110 @@ for (Xpotrf_buffer, Xpotrf, Xtrsm, Xgemm, T) in (
                 end
             end
         end
+
+        function cholesky_solve!(A_ptrs::CuVector{<:CuPtr{$T}}, B_ptrs::CuVector{<:CuPtr{$T}}, d_ptrs::CuVector{<:CuPtr{$T}}, N, n, nd)
+
+            CUBLAS.$Xtrsm(CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT, n, nd, one($T), A_ptrs[1], n, d_ptrs[1], n)
+        
+            for i = 2:N
+                CUBLAS.$Xgemm(CUBLAS.handle(), CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_OP_N,
+                    n, nd, n, -one($T), B_ptrs[i-1], n, d_ptrs[i-1], n, one($T), d_ptrs[i], n)
+                CUBLAS.$Xtrsm(CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                    CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT, n, nd, one($T), A_ptrs[i], n, d_ptrs[i], n)
+            end
+        
+            CUBLAS.$Xtrsm(CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_DIAG_NON_UNIT, n, nd, one($T), A_ptrs[N], n, d_ptrs[N], n)
+        
+            for i = N-1:-1:1
+                CUBLAS.$Xgemm(CUBLAS.handle(), CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_OP_N,
+                    n, nd, n, -one($T), B_ptrs[i], n, d_ptrs[i+1], n, one($T), d_ptrs[i], n)
+                CUBLAS.$Xtrsm(CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                    CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_DIAG_NON_UNIT, n, nd, one($T), A_ptrs[i], n, d_ptrs[i], n)
+            end
+        
+        end
     end
 end
 
-function cholesky_solve!(A_ptrs::CuVector{<:CuPtr{T}}, B_ptrs::CuVector{<:CuPtr{T}}, d_ptrs::CuVector{<:CuPtr{T}}, N, n, nd) where {T<:Union{Float32,Float64}}
+for (XpotrfBatched, XtrsmBatched, XgemmBatched, T) in (
+    (:cusolverDnSpotrfBatched, :cublasStrsmBatched, :cublasSgemmBatched, :Float32),
+    (:cusolverDnDpotrfBatched, :cublasDtrsmBatched, :cublasDgemmBatched, :Float64),
+)
+    @eval begin
+        function cholesky_factorize_batched!(A_ptrs::CuVector{<:CuPtr{$T}}, B_ptrs::CuVector{<:CuPtr{$T}}, bsz, sep, start, N, n)
 
-    CUBLAS.cublasDtrsm_v2(CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-        CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT, n, nd, one(T), A_ptrs[1], n, d_ptrs[1], n)
+            # ---- one info buffer and one cuSOLVER handle ---------------------------
+            dh = CUSOLVER.dense_handle()
+            CUDA.resize!(dh.info, bsz)
 
-    for i = 2:N
-        CUBLAS.cublasDgemm_v2(CUBLAS.handle(), CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_OP_N,
-            n, nd, n, -one(T), B_ptrs[i-1], n, d_ptrs[i-1], n, one(T), d_ptrs[i], n)
-        CUBLAS.cublasDtrsm_v2(CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-            CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT, n, nd, one(T), A_ptrs[i], n, d_ptrs[i], n)
+            # ---- first block row ----------------------------------------------------
+            CUSOLVER.$XpotrfBatched(
+                dh, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                n, A_ptrs[start:sep:end], n, dh.info, bsz)
+            
+            # ---- remaining block rows ----------------------------------------------
+            for i in (start+1):(start+N-1)
+
+                CUBLAS.$XtrsmBatched(
+                    CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                    CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT,
+                    n, n, one($T), A_ptrs[i-1:sep:end], n, B_ptrs[(i-1):sep:end], n, bsz)
+
+                CUBLAS.$XgemmBatched(
+                    CUBLAS.handle(), CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_OP_N,
+                    n, n, n, -one($T),
+                    B_ptrs[(i-1):sep:end], n, B_ptrs[(i-1):sep:end], n,
+                    one($T), A_ptrs[i:sep:end], n, bsz)
+
+                CUSOLVER.$XpotrfBatched(
+                    dh, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                    n, A_ptrs[i:sep:end], n, dh.info, bsz)
+
+            end
+            
+        end
+
+        function cholesky_solve_batched!(A_ptrs::CuVector{<:CuPtr{$T}}, B_ptrs::CuVector{<:CuPtr{$T}}, d_ptrs::CuVector{<:CuPtr{$T}}, bsz, sep, start, N, n, nd)
+
+            CUBLAS.$XtrsmBatched(
+                CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT,
+                n, nd, one($T), A_ptrs[start:sep:end], n, d_ptrs[start:sep:end], n, bsz)
+
+            for i = (start+1):(start+N-1)
+                CUBLAS.$XgemmBatched(
+                    CUBLAS.handle(), CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_OP_N,
+                    n, nd, n, -one($T),
+                    B_ptrs[(i-1):sep:end], n, d_ptrs[(i-1):sep:end], n,
+                    one($T), d_ptrs[i:sep:end], n, bsz)
+
+                CUBLAS.$XtrsmBatched(
+                    CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                    CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT,
+                    n, nd, one($T), A_ptrs[i:sep:end], n, d_ptrs[i:sep:end], n, bsz)
+            end
+
+            for i = (start+N-1):-1:(start+1)
+                CUBLAS.$XtrsmBatched(
+                    CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                    CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_DIAG_NON_UNIT,
+                    n, nd, one($T), A_ptrs[i:sep:end], n, d_ptrs[i:sep:end], n, bsz)
+
+                CUBLAS.$XgemmBatched(
+                    CUBLAS.handle(), CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_OP_N,
+                    n, nd, n, -one($T),
+                    B_ptrs[(i-1):sep:end], n, d_ptrs[i:sep:end], n,
+                    one($T), d_ptrs[(i-1):sep:end], n, bsz)
+            end
+
+            CUBLAS.$XtrsmBatched(
+                CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
+                CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_DIAG_NON_UNIT,
+                n, nd, one($T), A_ptrs[start:sep:end], n, d_ptrs[start:sep:end], n, bsz)    
+
+        end
+
     end
-
-    CUBLAS.cublasDtrsm_v2(CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-        CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_DIAG_NON_UNIT, n, nd, one(T), A_ptrs[N], n, d_ptrs[N], n)
-
-    for i = N-1:-1:1
-        CUBLAS.cublasDgemm_v2(CUBLAS.handle(), CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_OP_N,
-            n, nd, n, -one(T), B_ptrs[i], n, d_ptrs[i+1], n, one(T), d_ptrs[i], n)
-        CUBLAS.cublasDtrsm_v2(CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-            CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_DIAG_NON_UNIT, n, nd, one(T), A_ptrs[i], n, d_ptrs[i], n)
-    end
-
-end
-
-function cholesky_factorize_batched!(A_ptrs::CuVector{<:CuPtr{T}}, B_ptrs::CuVector{<:CuPtr{T}}, bsz, sep, start, N, n) where {T<:Union{Float32,Float64}}
-
-    # ---- one info buffer and one cuSOLVER handle ---------------------------
-    dh = CUSOLVER.dense_handle()
-    CUDA.resize!(dh.info, bsz)
-
-    # ---- first block row ----------------------------------------------------
-    CUSOLVER.cusolverDnDpotrfBatched(
-        dh, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-        n, A_ptrs[start:sep:end], n, dh.info, bsz)
-    
-    # ---- remaining block rows ----------------------------------------------
-    for i in (start+1):(start+N-1)
-
-        CUBLAS.cublasDtrsmBatched(
-            CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-            CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT,
-            n, n, one(T), A_ptrs[i-1:sep:end], n, B_ptrs[(i-1):sep:end], n, bsz)
-
-        CUBLAS.cublasDgemmBatched(
-            CUBLAS.handle(), CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_OP_N,
-            n, n, n, -one(T),
-            B_ptrs[(i-1):sep:end], n, B_ptrs[(i-1):sep:end], n,
-            one(T), A_ptrs[i:sep:end], n, bsz)
-
-        CUSOLVER.cusolverDnDpotrfBatched(
-            dh, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-            n, A_ptrs[i:sep:end], n, dh.info, bsz)
-
-    end
-    
-end
-
-function cholesky_solve_batched!(A_ptrs::CuVector{<:CuPtr{T}}, B_ptrs::CuVector{<:CuPtr{T}}, d_ptrs::CuVector{<:CuPtr{T}}, bsz, sep, start, N, n, nd) where {T<:Union{Float32,Float64}}
-
-    CUBLAS.cublasDtrsmBatched(
-        CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-        CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT,
-        n, nd, one(T), A_ptrs[start:sep:end], n, d_ptrs[start:sep:end], n, bsz)
-
-    for i = (start+1):(start+N-1)
-        CUBLAS.cublasDgemmBatched(
-            CUBLAS.handle(), CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_OP_N,
-            n, nd, n, -one(T),
-            B_ptrs[(i-1):sep:end], n, d_ptrs[(i-1):sep:end], n,
-            one(T), d_ptrs[i:sep:end], n, bsz)
-
-        CUBLAS.cublasDtrsmBatched(
-            CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-            CUBLAS.CUBLAS_OP_T, CUBLAS.CUBLAS_DIAG_NON_UNIT,
-            n, nd, one(T), A_ptrs[i:sep:end], n, d_ptrs[i:sep:end], n, bsz)
-    end
-
-    for i = (start+N-1):-1:(start+1)
-        CUBLAS.cublasDtrsmBatched(
-            CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-            CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_DIAG_NON_UNIT,
-            n, nd, one(T), A_ptrs[i:sep:end], n, d_ptrs[i:sep:end], n, bsz)
-
-        CUBLAS.cublasDgemmBatched(
-            CUBLAS.handle(), CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_OP_N,
-            n, nd, n, -one(T),
-            B_ptrs[(i-1):sep:end], n, d_ptrs[i:sep:end], n,
-            one(T), d_ptrs[(i-1):sep:end], n, bsz)
-    end
-
-    CUBLAS.cublasDtrsmBatched(
-        CUBLAS.handle(), CUBLAS.CUBLAS_SIDE_LEFT, CUBLAS.CUBLAS_FILL_MODE_UPPER,
-        CUBLAS.CUBLAS_OP_N, CUBLAS.CUBLAS_DIAG_NON_UNIT,
-        n, nd, one(T), A_ptrs[start:sep:end], n, d_ptrs[start:sep:end], n, bsz)    
-
 end
