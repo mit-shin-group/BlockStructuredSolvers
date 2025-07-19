1207d0883f010ffc6d68935bf5ccb8f88bb9d92d
jindavid <daaaaavjd@gmail.com>
2025-07-18 21:26:10 -0400

    update CPU struct

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -18,9 +18,10 @@ device!(1)
 
 println(LinearAlgebra.BLAS.get_config())
 
-N = 100
-n = 32 # size of each block
+N = 32
+n = 54 # size of each block
 seed = 42 # random seed for reproducibility
+T = Float64
 
 function run(N, n)
     # Set random seed for reproducibility
@@ -31,33 +32,43 @@ function run(N, n)
     BigMatrix, d = construct_block_tridiagonal(A_list, B_list, d_list)
 
     # MA57
-    BigMatrix_57 = Ma57(BigMatrix)
-    d_57 = deepcopy(d)
-    ma57_factorize_time = @elapsed ma57_factorize!(BigMatrix_57)
-    ma57_solve_time = @elapsed ma57_solve(BigMatrix_57, d_57)
-    BigMatrix_57 = nothing
+    # BigMatrix_57 = Ma57(BigMatrix)
+    # d_57 = deepcopy(d)
+    # ma57_factorize_time = @elapsed ma57_factorize!(BigMatrix_57)
+    # ma57_solve_time = @elapsed ma57_solve(BigMatrix_57, d_57)
+    # BigMatrix_57 = nothing
 
-    # CHOLMOD
-    chol_factor_time = @elapsed F = cholesky(BigMatrix)
-    chol_solve_time = @elapsed F \ d
-    F = nothing
+    # # CHOLMOD
+    # chol_factor_time = @elapsed F = cholesky(BigMatrix)
+    # chol_solve_time = @elapsed F \ d
+    # F = nothing
 
-    # LDLFactorizations 
-    ldl_factor_time = @elapsed LDLT = ldl(BigMatrix; P=Vector(1:N*n))
-    ldl_solve_time = @elapsed LDLT \ d
-    LDLT = nothing
+    # # LDLFactorizations 
+    # ldl_factor_time = @elapsed LDLT = ldl(BigMatrix; P=Vector(1:N*n))
+    # ldl_solve_time = @elapsed LDLT \ d
+    # LDLT = nothing
 
     # CUDSS
     BigMatrix_cudss = CUSPARSE.CuSparseMatrixCSR(BigMatrix)
+    BigMatrix_cudss = CUDSS.CudssMatrix(BigMatrix_cudss, "SPD", 'U')
+    config = CUDSS.CudssConfig()
+    data = CUDSS.CudssData()
+    solver = CUDSS.CudssSolver(BigMatrix_cudss, config, data)
     d_cudss = CuArray(d)
-    chol = CUDSS.cholesky(BigMatrix_cudss; check=false)
-    cudss_factor_time = CUDA.@elapsed blocking=true CUDSS.cholesky(BigMatrix_cudss; check=false)
+    x_cudss = CUDA.zeros(T, N*n, 1)
+    b_cudss = CUDA.zeros(T, N*n, 1)
+    cudss_analysis_time = CUDA.@elapsed CUDSS.cudss("analysis", solver, x_cudss, b_cudss)
+    # chol = CUDSS.cholesky(BigMatrix_cudss; check=false)
+    # cudss_factor_time = CUDA.@elapsed blocking=true CUDSS.cholesky(BigMatrix_cudss; check=false)
+    cudss_factor_time = CUDA.@elapsed CUDSS.cudss("factorization", solver, x_cudss, b_cudss)
+    # cudss_factor_time += cudss_analysis_time
+    cudss_solve_time = CUDA.@elapsed CUDSS.cudss("solve", solver, x_cudss, d_cudss)
     
     # Time CUDSS solve using events
-    x_cudss = deepcopy(d_cudss)
-    cudss_solve_time = CUDA.@elapsed blocking=true cudss("solve", chol, x_cudss, d_cudss)
-    BigMatrix_cudss = nothing
-    chol = nothing
+    # x_cudss = deepcopy(d_cudss)
+    # cudss_solve_time = CUDA.@elapsed blocking=true cudss("solve", chol, x_cudss, d_cudss)
+    # BigMatrix_cudss = nothing
+    # chol = nothing
 
     # Create GPU tensors for our algorithms
     A_tensor_gpu = CuArray{Float64}(undef, n, n, N)
@@ -78,14 +89,14 @@ function run(N, n)
     end
     
     # Initialize batched solver
-    data_gpu = initialize_batched(N, n)
+    data_gpu = initialize_batched(N, n, Float64, CuArray)
     # Copy data to GPU tensors
     copyto!(data_gpu.A_tensor, A_tensor_gpu)
     copyto!(data_gpu.B_tensor, B_tensor_gpu)
     copyto!(data_gpu.d_tensor, d_tensor_gpu)
     
     # Initialize sequential solver
-    data_sequential = initialize_seq(N, n)
+    data_sequential = initialize_seq(N, n, Float64, CuArray)
     # Copy data to GPU tensors
     copyto!(data_sequential.A_tensor, A_tensor_gpu)
     copyto!(data_sequential.B_tensor, B_tensor_gpu)
@@ -108,12 +119,19 @@ function run(N, n)
     GC.gc()
     CUDA.reclaim()
 
-    results = [ma57_factorize_time, 
-    ma57_solve_time, 
-    chol_factor_time, 
-    chol_solve_time, 
-    ldl_factor_time, 
-    ldl_solve_time, 
+    results = [
+    # ma57_factorize_time, 
+    # ma57_solve_time, 
+    # chol_factor_time, 
+    # chol_solve_time, 
+    # ldl_factor_time, 
+    # ldl_solve_time, 
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
     cudss_factor_time, 
     cudss_solve_time, 
     gpu_factorize_time, 
@@ -149,7 +167,7 @@ function benchmark_factorization_and_solve(iter)
 
     _ = run(20, 32)
 
-    (N, n) = (51, 129)
+    # (N, n) = (51, 129)
 
     println("Starting benchmark...")
 
@@ -179,9 +197,9 @@ function benchmark_factorization_and_solve(iter)
     # Compute and print the average times
     println("\nAverage Factorization and Solve Times over ", iter, " Runs:", " N: $N, n: $n")
     println("---------------------------------------------------")
-    @printf("MA57 - Factorize: %.6f ms, Solve: %.6f ms\n", mean(ma57_factor_times) * 1000, mean(ma57_solve_times) * 1000)
-    @printf("CHOLMOD - Factorize: %.6f ms, Solve: %.6f ms\n", mean(chol_factor_times) * 1000, mean(chol_solve_times) * 1000)
-    @printf("LDLFactorizations - Factorize: %.6f ms, Solve: %.6f ms\n", mean(ldl_factor_times) * 1000, mean(ldl_solve_times) * 1000)
+    # @printf("MA57 - Factorize: %.6f ms, Solve: %.6f ms\n", mean(ma57_factor_times) * 1000, mean(ma57_solve_times) * 1000)
+    # @printf("CHOLMOD - Factorize: %.6f ms, Solve: %.6f ms\n", mean(chol_factor_times) * 1000, mean(chol_solve_times) * 1000)
+    # @printf("LDLFactorizations - Factorize: %.6f ms, Solve: %.6f ms\n", mean(ldl_factor_times) * 1000, mean(ldl_solve_times) * 1000)
     @printf("CUDSS - Factorize: %.6f ms, Solve: %.6f ms\n", mean(cudss_factor_times) * 1000, mean(cudss_solve_times) * 1000)
     @printf("GPU Batched - Factorize: %.6f ms, Solve: %.6f ms\n", mean(gpu_factor_times) * 1000, mean(gpu_solve_times) * 1000)
     @printf("GPU Sequential - Factorize: %.6f ms, Solve: %.6f ms\n", mean(sequential_factor_times) * 1000, mean(sequential_solve_times) * 1000)
@@ -189,9 +207,9 @@ function benchmark_factorization_and_solve(iter)
     # Also print standard deviations
     println("\nStandard Deviations:")
     println("---------------------------------------------------")
-    @printf("MA57 - Factorize: %.6f ms, Solve: %.6f ms\n", std(ma57_factor_times) * 1000, std(ma57_solve_times) * 1000)
-    @printf("CHOLMOD - Factorize: %.6f ms, Solve: %.6f ms\n", std(chol_factor_times) * 1000, std(chol_solve_times) * 1000)
-    @printf("LDLFactorizations - Factorize: %.6f ms, Solve: %.6f ms\n", std(ldl_factor_times) * 1000, std(ldl_solve_times) * 1000)
+    # @printf("MA57 - Factorize: %.6f ms, Solve: %.6f ms\n", std(ma57_factor_times) * 1000, std(ma57_solve_times) * 1000)
+    # @printf("CHOLMOD - Factorize: %.6f ms, Solve: %.6f ms\n", std(chol_factor_times) * 1000, std(chol_solve_times) * 1000)
+    # @printf("LDLFactorizations - Factorize: %.6f ms, Solve: %.6f ms\n", std(ldl_factor_times) * 1000, std(ldl_solve_times) * 1000)
     @printf("CUDSS - Factorize: %.6f ms, Solve: %.6f ms\n", std(cudss_factor_times) * 1000, std(cudss_solve_times) * 1000)
     @printf("GPU Batched - Factorize: %.6f ms, Solve: %.6f ms\n", std(gpu_factor_times) * 1000, std(gpu_solve_times) * 1000)
     @printf("GPU Sequential - Factorize: %.6f ms, Solve: %.6f ms\n", std(sequential_factor_times) * 1000, std(sequential_solve_times) * 1000)
--- test/test_cpu.jl
+++ test/test_cpu.jl
@@ -9,63 +9,59 @@ end
 function run_cpu_test(N, n, T)
     println("Testing CPU solver with $(T) precision")
     
-    # Initialize the solver
-    data = initialize_cpu(N, n, T)
-    
-    # Create test data
-    A_tensor = Array{T, 3}(zeros(n, n, N))
-    B_tensor = Array{T, 3}(zeros(n, n, N-1))
-    
-    # Generate positive definite A matrices and random B matrices
+    A_list = Vector{Matrix{T}}(undef, N);
+    B_list = Vector{Matrix{T}}(undef, N-1);
+
     for i in 1:N
-        temp = randn(T, n, n)
-        A_tensor[:, :, i] = temp * temp' + n * I
+        temp = randn(n, n)
+        A_list[i] = Matrix(temp * temp' + n * I) 
     end
-    
+
     for i in 1:N-1
-        temp = randn(T, n, n)
-        B_tensor[:, :, i] = temp
+        temp = randn(n, n)
+        B_list[i] = Matrix(temp)
     end
-    
-    # Copy data to solver
+
+    d_list = Vector{Matrix{T}}(undef, N);
+
+    x_list = Vector{Matrix{T}}(undef, N);
     for i in 1:N
-        copyto!(data.A_list[i], A_tensor[:, :, i])
+        x_list[i] = Matrix{T}(rand(n, 1))
     end
-    
-    for i in 1:N-1
-        copyto!(data.B_list[i], B_tensor[:, :, i])
-    end
-    
-    # Create solution vector x and right-hand side d
-    x_list = Vector{Matrix{T}}(undef, N)
-    for i in 1:N
-        x_list[i] = rand(T, n, 1)
-    end
-    
-    # Compute right-hand side d = Ax
-    d_list = Vector{Matrix{T}}(undef, N)
-    d_list[1] = A_tensor[:, :, 1] * x_list[1] + B_tensor[:, :, 1] * x_list[2]
+
+    d_list[1] = A_list[1] * x_list[1] + B_list[1] * x_list[2];
     @views for i = 2:N-1
-        d_list[i] = B_tensor[:, :, i-1]' * x_list[i-1] + A_tensor[:, :, i] * x_list[i] + B_tensor[:, :, i] * x_list[i+1]
+        d_list[i] = B_list[i-1]' * x_list[i-1] + A_list[i] * x_list[i] + B_list[i] * x_list[i+1]
     end
-    d_list[N] = B_tensor[:, :, N-1]' * x_list[N-1] + A_tensor[:, :, N] * x_list[N]
-    
-    # Save original right-hand side for later comparison
-    d_orig = deepcopy(d_list)
+    d_list[N] = B_list[N-1]' * x_list[N-1] + A_list[N] * x_list[N];
+
+    solver = initialize_cpu(N, n, T);
+
+    for i in 1:N
+        copyto!(solver.A_list[i], A_list[i])
+    end
+
+    for i in 1:N-1
+        copyto!(solver.B_list[i], B_list[i])
+    end
+
+    for i in 1:N
+        copyto!(solver.d_list[i], d_list[i])
+    end
     
     # Calculate error tolerance based on precision
     ϵ = sqrt(eps(T))
     
     # Factorize
     println("  Factorization:")
-    @time factorize!(data)
+    @time factorize!(solver)
     
     # Solve
     println("  Solve:")
-    @time solve!(data, d_list)
+    @time solve!(solver)
     
     # Compute error
-    error_vec = [d_list[i] - x_list[i] for i in 1:N]
+    error_vec = [solver.d_list[i] - x_list[i] for i in 1:N]
     error = _bss_norm(error_vec)
     
     # Compute norm of the right-hand side for scaling
@@ -76,30 +72,24 @@ function run_cpu_test(N, n, T)
     
     # Also test that A*x_solved = b by computing the residual
     # Save the solution
-    x_solved = deepcopy(d_list)
-    
-    # Restore the right-hand side
-    d_list = deepcopy(d_orig)
-    
+    x_solved = solver.d_list
+
     # Compute the residual A*x_solved - b
     res_list = Vector{Matrix{T}}(undef, N)
-    res_list[1] = A_tensor[:, :, 1] * x_solved[1] + B_tensor[:, :, 1] * x_solved[2] - d_list[1]
+    res_list[1] = A_list[1] * x_solved[1] + B_list[1] * x_solved[2] - d_list[1]
     @views for i = 2:N-1
-        res_list[i] = B_tensor[:, :, i-1]' * x_solved[i-1] + 
-                       A_tensor[:, :, i] * x_solved[i] + 
-                       B_tensor[:, :, i] * x_solved[i+1] - 
+        res_list[i] = B_list[i-1]' * x_solved[i-1] + 
+                       A_list[i] * x_solved[i] + 
+                       B_list[i] * x_solved[i+1] - 
                        d_list[i]
     end
-    res_list[N] = B_tensor[:, :, N-1]' * x_solved[N-1] + A_tensor[:, :, N] * x_solved[N] - d_list[N]
+    res_list[N] = B_list[N-1]' * x_solved[N-1] + A_list[N] * x_solved[N] - d_list[N]
     
     # Compute the residual norm
     res_norm = _bss_norm(res_list)
     
     # Test if residual is small
     @test res_norm ≤ ϵ * _bss_norm(d_list) * 10
-    
-    println("  Relative solution error: $(error/rhs_norm)")
-    println("  Relative residual: $(res_norm/_bss_norm(d_list))")
     
     return error, rhs_norm, res_norm, _bss_norm(d_list)
 end
