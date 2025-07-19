c63c10b109c0e3a9cdd7aa5fa43f49afd1ee4a37
jindavid <daaaaavjd@gmail.com>
2025-07-18 20:29:13 -0400

    update CPU struct

--- src/cpu.jl
+++ src/cpu.jl
@@ -5,6 +5,7 @@ struct BlockTriDiagData_cpu{T}
 
     A_list::Vector{Array{T, 2}}
     B_list::Vector{Array{T, 2}}
+    d_list::Vector{Array{T, 2}}
 
 end
 
@@ -12,12 +13,14 @@ function initialize_cpu(N, n, ::Type{T}) where T
 
     A_list = [Array{T, 2}(zeros(n, n)) for i in 1:N]
     B_list = [Array{T, 2}(zeros(n, n)) for i in 1:N-1]
+    d_list = [Array{T, 2}(zeros(n, 1)) for i in 1:N]
 
     data = BlockTriDiagData_cpu(
         N,
         n,
         A_list,
-        B_list
+        B_list,
+        d_list
     )
 
     return data
@@ -35,12 +38,13 @@ function factorize!(data::BlockTriDiagData_cpu)
 
 end
 
-function solve!(data::BlockTriDiagData_cpu, d_list)
+function solve!(data::BlockTriDiagData_cpu)
 
     N = data.N
     A_list = data.A_list
     B_list = data.B_list
-
+    d_list = data.d_list
+    
     cholesky_solve!(A_list, B_list, d_list, N)
 
 end
